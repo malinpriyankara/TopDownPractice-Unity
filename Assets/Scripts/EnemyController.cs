136bfab7dbaaa49f2bc5ca8a119c95cea5419661
Malin Priyankara <malinpriyankara86@gmail.com>
2023-01-30 16:33:07 +0530

    1225

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -4,19 +4,16 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
-    [SerializeField]
-    private GameObject _player;
-    [SerializeField]
-    private float _enemySpeed = 2f;
-    [SerializeField]
-    private float _range = 1f;
-    [SerializeField]
-    private float _minDistance = 1f;
+    [SerializeField] private GameObject _player;
+    [SerializeField] private float _enemySpeed = 2f;
+    [SerializeField] private float _range = 1f;
+    [SerializeField] private float _minDistance = 1f;
     [SerializeField] private GameObject _bullet;
     [SerializeField] private GameObject _gun;
+    [SerializeField] private AudioSource _audio001;
     void Start()
     {
-        
+        _audio001.enabled = true;
     }//Start
 
     
@@ -37,10 +34,12 @@ public class EnemyController : MonoBehaviour
     }//Player chase
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        
+       if(collision.gameObject.CompareTag("Bullet")) _audio001.Play(0);
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Bullet")) Destroy(this.gameObject, 0);
-    }
+       
+    }//On trigger enter
+   
 }//Class
